03399bd5674e942d3c6e88c78d892cebd9c39838
Amit Kumar Yadav <ameykumar76@gmail.com>
2026-05-02 10:50:31 +0530

    Update allowed origins in runtime configuration to include new subdomains for enhanced CORS support.

--- config/runtimeConfig.js
+++ config/runtimeConfig.js
@@ -2,7 +2,7 @@ const runtimeConfig = {
   PORT: '4000',
   MONGO_URI: 'mongodb://seo_panel:9315264682@ac-fv7uqzx-shard-00-00.wbfh1zj.mongodb.net:27017,ac-fv7uqzx-shard-00-01.wbfh1zj.mongodb.net:27017,ac-fv7uqzx-shard-00-02.wbfh1zj.mongodb.net:27017/?ssl=true&replicaSet=atlas-xlg8mn-shard-0&authSource=admin&appName=Cluster0',
   ALLOWED_ORIGINS:
-    'http://localhost:3000,https://www.seedsofinnocens.com,https://www.seedsofinnocence.com,https://soi.seedsofinnocens.com',
+    'http://localhost:3000,https://www.seedsofinnocens.com,https://www.seedsofinnocence.com,https://seeds.seedsofinnocens.com,https://soi-admin.seedsofinnocens.com',
   RECEIVER_EMAIL: 'digital@seedsofinnocence.com',
   FEEDBACK_RECEIVER_EMAIL: 'feedback@seedsofinnocence.com',
   SEO_AUTH_JWT_SECRET: 'change-this-seo-secret',
