6e8de7e14ead39f4bb17403cc87bd882ef91eec8
Amit Kumar Yadav <ameykumar76@gmail.com>
2026-07-24 16:50:53 +0530

    xrgcycd

--- controller/newWebsite/book-appointment.js
+++ controller/newWebsite/book-appointment.js
@@ -19,9 +19,7 @@ const LEADSQUARED_BASE_URL = cleanEnvValue(
 const LEADSQUARED_ENDPOINT = cleanEnvValue(
   process.env.LSQ_ENDPOINT || process.env.LEADSQUARED_ENDPOINT || process.env.LEADSQUARED_URL || ''
 );
-const LEADSQUARED_ACCESS_KEY = cleanLeadSquaredCredential(
-  process.env.LSQ_ACCESS_KEY || process.env.LEADSQUARED_ACCESS_KEY || runtimeConfig.LSQ_ACCESS_KEY
-);
+const LEADSQUARED_ACCESS_KEY = 'u$re4c970aee03a36630af47605bf4675fa';
 const LEADSQUARED_SECRET_KEY = cleanLeadSquaredCredential(
   process.env.LSQ_SECRET_KEY || process.env.LEADSQUARED_SECRET_KEY || runtimeConfig.LSQ_SECRET_KEY
 );
